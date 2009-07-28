06c568b30d04bb07c4f01b790384030a27bc0148
Drew Neil <a.neil@pccl.co.uk>
2009-07-28 15:50:07 +0100

    Added a issues/index view. Fixed an error with ApplicationController.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,13 @@
 class IssuesController < ApplicationController
+  # Prevent error: 'A copy of ApplicationController has been removed
+  # from the module tree but is still active!'
+  #   http://tinyurl.com/nzu2y2
+  unloadable
+
+  before_filter :require_user
+  before_filter :set_current_tab, :only => [:index]
+
+  def index
+  end
+
 end
