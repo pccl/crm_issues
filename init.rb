8ecdb06a74f7f06fc80675ddc949581cdabec9c1
Drew Neil <a.neil@pccl.co.uk>
2009-07-28 15:31:40 +0100

    Added 'Issues' tab to layout.

--- init.rb
+++ init.rb
@@ -1,1 +1,7 @@
 # Include hook code here
+#
+tabs = Setting.tabs
+unless tabs.map{|t| t[:url] }.any? { |url| url.match(/issues$/) }
+  tabs << {:url => '/issues', :text => 'Issues', :active => false}
+  Setting.tabs = tabs
+end
