f47fcf311366fb379244c70c55c3a8a2b424c79b
Drew Neil <a.neil@pccl.co.uk>
2009-07-29 10:41:49 +0100

    Removed a debugger call.

--- init.rb
+++ init.rb
@@ -1,6 +1,5 @@
 # Include hook code here
 #
-#require 'ruby-debug'; debugger
 if ActiveRecord::Base.connection.tables.include?('settings')
   if tabs = Setting.tabs
     unless tabs.map{|t| t[:url] }.any? { |url| url.match(/issues$/) }
