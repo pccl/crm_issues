efd3bf95d727be19b5ca6187d7e231ea0a15b14c
Andrew Neil <an@dev-p.int.pccl>
2009-07-29 09:57:42 +0100

    'Issues' tab should not interfere with fresh db.

--- init.rb
+++ init.rb
@@ -1,7 +1,11 @@
 # Include hook code here
 #
-tabs = Setting.tabs
-unless tabs.map{|t| t[:url] }.any? { |url| url.match(/issues$/) }
-  tabs << {:url => '/issues', :text => 'Issues', :active => false}
-  Setting.tabs = tabs
+#require 'ruby-debug'; debugger
+if ActiveRecord::Base.connection.tables.include?('settings')
+  if tabs = Setting.tabs
+    unless tabs.map{|t| t[:url] }.any? { |url| url.match(/issues$/) }
+      tabs << {:url => '/issues', :text => 'Issues', :active => false}
+      Setting.tabs = tabs
+    end
+  end
 end
