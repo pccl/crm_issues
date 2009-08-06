cc9c7d494c0e1794534c4dea3a1d8e0e41ef7993
Drew Neil <a.neil@pccl.co.uk>
2009-08-06 11:26:38 +0100

    MIGRATION removed booleans, and created Status.

--- db/migrate/20090728145946_create_issues.rb
+++ db/migrate/20090728145946_create_issues.rb
@@ -3,8 +3,7 @@ class CreateIssues < ActiveRecord::Migration
     create_table :issues do |t|
       t.string   :bug_ticket, :summary, :priority
       t.string   :access, :limit => 8, :default => "Private" # %w(Private Public Shared)
-      t.boolean  :bug_resolved, :issue_resolved
-      t.integer  :user_id, :assigned_to
+      t.integer  :user_id, :assigned_to, :status
       t.date :due_on
       t.datetime :deleted_at
 
