4cc455f834bd9740397ba7aa865088de9519a944
Drew Neil <a.neil@pccl.co.uk>
2009-08-11 14:36:08 +0100

    Filter by priority now triggered by clicking checkboxes.

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -2,7 +2,11 @@ module IssuesHelper
 
   def issue_priority_checkbox(priority, count=0)
     checked = false
-    check_box_tag("priority[]", priority, checked)
+    check_box_tag("priority[]", priority, checked,
+      :onclick => remote_function(:url => { :action => :filter }, 
+        :with => %Q/"priority=" + $$("input[name='priority[]']").findAll(function (el) { return el.checked }).pluck("value")/
+      )
+    )
   end
 
 end
