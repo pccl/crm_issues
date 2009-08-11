64d14ed769336a2d989940cd7a81c2c4e21195c8
Drew Neil <a.neil@pccl.co.uk>
2009-08-11 14:46:41 +0100

    Check boxes are properly initialised from session.

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,7 +1,13 @@
 module IssuesHelper
 
   def issue_priority_checkbox(priority, count=0)
-    checked = false
+
+    if session[:filter_by_issue_priority]
+      checked = session[:filter_by_issue_priority].split(",").include?(priority.to_s)
+    else
+      checked = count > 0
+    end
+
     check_box_tag("priority[]", priority, checked,
       :onclick => remote_function(:url => { :action => :filter }, 
         :with => %Q/"priority=" + $$("input[name='priority[]']").findAll(function (el) { return el.checked }).pluck("value")/
