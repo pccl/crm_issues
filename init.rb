9177641ef00ee187c02d125e73f2fb8d88e36ce7
Drew Neil <a.neil@pccl.co.uk>
2009-08-19 09:45:19 +0100

    Use Dispatcher.to_prepare to reload modules in dev mode.
    Thanks to Michael Dvorkin for his help in this:
        http://github.com/michaeldv/fat_free_crm/commit/33a2b826e874be7975e298273f9959d891283cc0

--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 require "fat_free_crm"
+require 'crm_issues'
+
 FatFreeCRM::Plugin.register(:crm_issues, initializer) do
           name "Fat Free Issues"
         author "Drew Neil"
@@ -7,22 +9,3 @@ FatFreeCRM::Plugin.register(:crm_issues, initializer) do
   dependencies :simple_column_search, :uses_mysql_uuid, :uses_user_permissions
 end
 
-require 'crm_issues'
-# Tip taken from here:
-#   http://www.spacevatican.org/2008/5/28/reload-me-reload-me-not
-# Without this line, development mode has a strange bug where associations
-# between models in the plugin work fine for a single request after restarting
-# the server, but fail to work on subsequent requests.
-ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__) + '/../app/models'))
-
-if ActiveRecord::Base.connection.tables.include?('settings')
-  if tabs = Setting.tabs
-    unless tabs.map{|t| t[:url][:controller] }.any? { |url| url.match(/issues$/) unless url.blank? }
-      tabs << {:url => { :controller => 'issues' }, :text => 'Issues', :active => false}
-      Setting.tabs = tabs
-    end
-  end
-end
-
-# Let the comments_controller know that issues are commentable
-CommentsController::COMMENTABLE = CommentsController::COMMENTABLE + %w(issue_id)
--- lib/crm_issues.rb
+++ lib/crm_issues.rb
@@ -1,10 +1,24 @@
-# crm_issues.rb
+require "dispatcher"
 
-# Extend :account model to add :issues association.
-Account.send(:include, AccountIssueAssociations)
+Dispatcher.to_prepare do
+  
+  # Extend :account model to add :issues association.
+  Account.send(:include, AccountIssueAssociations)
 
-# Let the activity_observer know about issues.
-ActivityObserver.instance.send :add_observer!, Issue
+  # Make issues observable.
+  ActivityObserver.instance.send :add_observer!, Issue
 
-# Add :issues plugin helpers
-ActionView::Base.send(:include, IssuesHelper)
+  # Add :issues plugin helpers.
+  ActionView::Base.send(:include, IssuesHelper)
+
+end
+
+# NOTE: for some misterious reason the following doesn't work within Dispatcher block,
+# so the :tabs override works in production mode only. (Or in development mode with 
+# config.cache_classes = true.)
+
+# Override :tabs in the application helper.
+ActionView::Helpers::ApplicationHelper.send(:include, TabsHelper)
+
+# Make the issues commentable.
+CommentsController::COMMENTABLE = CommentsController::COMMENTABLE + %w(issue_id)
