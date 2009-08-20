d670c6f34e05ce8b1ddb680a069f046f147d35ee
Drew Neil <a.neil@pccl.co.uk>
2009-08-20 14:49:43 +0100

    Added haml to plugin dependencies, and require hooks.

--- init.rb
+++ init.rb
@@ -1,11 +1,13 @@
 require "fat_free_crm"
-require 'crm_issues'
 
 FatFreeCRM::Plugin.register(:crm_issues, initializer) do
           name "Fat Free Issues"
         author "Drew Neil"
        version "0.1"
    description "Basic issue tracking"
-  dependencies :simple_column_search, :uses_mysql_uuid, :uses_user_permissions
+  dependencies :haml, :simple_column_search, :uses_mysql_uuid, :uses_user_permissions
 end
 
+# Require the actual code after all plugin dependencies have been resoved.
+require 'crm_issues'
+require "show_account_hook"
